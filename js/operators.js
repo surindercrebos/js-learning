ffce8f3afa84dcfa9440127c5712f78f98d4b000
surindercrebos <surinder.crebos@gmail.com>
2024-07-18 13:30:30 +0530

    operators

---
+++ js/operators.js
@@ -0,0 +1,108 @@
+// Activity 1: Arithmetic Operations
+// * Task 1: Write a program to add two numbers and log the result to the console.
+let a = 10;
+let b = 15;
+let c = a + b;
+// console.log(`The sum of tow number is ${a} + ${b} =  ${c}`);
+
+// * Task 2: Write a program to subtract two numbers and log the result to the console.
+let d = 25;
+let e = 15;
+let f = d - e;
+// console.log(`The subtract of two number is ${d} - ${e} = ${f}`);
+
+// * Task 3: Write a program to multiply two numbers and log the result to the console.
+let g = 8;
+let h = 5;
+let i = g * h;
+// console.log(`The multiply of two number is ${g} * ${h} = ${i}`);
+
+// * Task 4: Write a program to divide two numbers and log the result to the console.
+let j = 20;
+let k = 5;
+let l = j / k;
+// console.log(`After divide of two number the result is ${j} / ${k} = ${l} `);
+
+// * Task 5: Write a program to find the remainder when one number is divided by another and log the result to the console.
+let m = 20;
+let n = 11;
+let o = m % n;
+// console.log(
+//   `Remainder when one number is divided by another is ${m} % ${n} = ${o}`
+// );
+
+// Activity 2: Assignment Operators
+// * Task 6: Use the += operator to add a number to a variable and log the result to the console.
+let p = 10;
+p += 5; // p = p+5
+// console.log(`Add a number to a variable with += operator is ${p}`);
+
+// * Task 7: Use the -= operator to subtract a number from a variable and log the result to the console.
+let q = 20;
+q -= 7; // q = q-5
+// console.log(`Subtract a number to a variable with += operator is ${q}`);
+
+// Activity 3: Comparison Operators
+// * Task 8: Write a program to compare two numbers using > and < and log the result to the console.
+let r = 18;
+let s = 16;
+// console.log(`${r > s}`);
+// if (r > s) {
+//     console.log(`${r} is greater than ${s}`);
+// } else if (r < s) {
+//     console.log(`${r} is less than ${s}`);
+// } else {
+//     console.log(`${r} is equal to ${s}`);
+// }
+
+let t = 18;
+let u = 20;
+// console.log(`${t < u}`);
+// if (t < u) {
+//     console.log(`${t} is less than ${u}`);
+// } else if (t > u) {
+//     console.log(`${t} is greater than ${u}`);
+// } else {
+//     console.log(`${t} is equal to ${u}`);
+// }
+
+// * Task 9: Write a program to compare two numbers using >= and <= and log the result to the console.
+let v = 32;
+let w = 31;
+// console.log(`${v >= w}`);
+// console.log(`${v <= w}`);
+// if (v >= w) {
+//   console.log(`${v} is greater than ${w}`);
+// } else if (v <= w) {
+//   console.log(`${v} is less than ${w}`);
+// }
+
+// * Task 10: Write a program to compare two numbers using == and === and log the result to the console.
+let x = 20;
+let y = "20";
+// console.log(`${x == y}`);
+// console.log(`${x === y}`);
+
+// Activity 4: Logical Operators
+// * Task 11: Write a program that uses the && operator to combine two conditions and log the result to the console.
+let num1 = 5;
+let num2 = 10;
+let result = num1 > 0 && num2 < 15;
+// console.log(`Result of (num1 > 0) && (num2 < 15): ${result}`);
+
+// * Task 12: Write a program that uses the || operator to combine two conditions and log the result to the console.
+let num3 = 10;
+let num4 = 15;
+let result2 = num3 > 0 || num4 < 20;
+// console.log(`Result of (num3 > 0) || (num4 < 20): ${result2}`);
+
+// * Task 13: Write a program that uses the ! operator to negate a condition and log the result to the console.
+let isSunny = true;
+let result3 = !isSunny;
+// console.log(`Result of !isSunny: ${result3}`);
+
+// Activity 5: Ternary Operator
+// Task 14: Write a program that uses the ternary operator to check if a number is positive or negative and log the result to the console.
+let num = -5;
+let result4 = num >= 0 ? "Positive" : "Negative";
+// console.log(`The number ${num} is ${result4}.`);
